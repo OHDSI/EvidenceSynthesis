8339e9223ca7f0fecbfc260c9e996a0c44b77c9c
Admin_mschuemi <Admin_mschuemi@its.jnj.com>
2022-12-23 06:37:53 -0500

    More work on video vignette

--- extras/VideoVignette.R
+++ extras/VideoVignette.R
@@ -1,6 +1,4 @@
-# Code used in the video vignette:
-
-# Simulate data ----------------------------------------------------------------
+# Code for generating plot for video vignette:
 library(EvidenceSynthesis)
 simulationSettings <- createSimulationSettings(nSites = 10,
                                                n = 10000,
@@ -11,48 +9,6 @@ simulationSettings <- createSimulationSettings(nSites = 10,
 set.seed(1)
 populations <- simulatePopulations(simulationSettings)
 
-head(populations[[1]])
-
-# Fit a model locally ----------------------------------------------------------
-library(Cyclops)
-# Assume we are at site 1:
-population <- populations[[1]]
-
-
-cyclopsData <- createCyclopsData(Surv(time, y) ~ x + strata(stratumId),
-                                 data = population,
-                                 modelType = "cox")
-cyclopsFit <- fitCyclopsModel(cyclopsData)
-
-# Hazard ratio:
-exp(coef(cyclopsFit))
-
-# 95% confidence interval:
-exp(confint(cyclopsFit, parm = "x")[2:3])
-
-# Approximate the likelihood function at one site ------------------------------
-approximation <-  approximateLikelihood(
-  cyclopsFit = cyclopsFit,
-  parameter = "x",
-  approximation = "normal"
-)
-plotLikelihoodFit(approximation = approximation,
-                  cyclopsFit = cyclopsFit,
-                  parameter = "x")
-
-approximation <-  approximateLikelihood(
-  cyclopsFit = cyclopsFit,
-  parameter = "x",
-  approximation = "adaptive grid",
-  bounds = c(log(0.1), log(10))
-)
-head(approximation)
-
-plotLikelihoodFit(approximation = approximation,
-                  cyclopsFit = cyclopsFit,
-                  parameter = "x")
-
-# Approximate at all sites -----------------------------------------------------
 fitModelInDatabase <- function(population, approximation) {
   cyclopsData <- createCyclopsData(Surv(time, y) ~ x + strata(stratumId),
                                    data = population,
@@ -64,16 +20,27 @@ fitModelInDatabase <- function(population, approximation) {
   return(approximation)
 }
 adaptiveGridApproximations <- lapply(populations, fitModelInDatabase, approximation = "adaptive grid")
-normalApproximations <- lapply(populations, fitModelInDatabase, approximation = "normal")
-normalApproximations <- dplyr::bind_rows(normalApproximations)
 
-# Synthesize evidence ----------------------------------------------------------
 
-computeFixedEffectMetaAnalysis(adaptiveGridApproximations)
-computeFixedEffectMetaAnalysis(normalApproximations)
-computeFixedEffectMetaAnalysis(populations)
 
-exp(computeBayesianMetaAnalysis(adaptiveGridApproximations)[, 1:3])
-exp(computeBayesianMetaAnalysis(normalApproximations)[, 1:3])
-exp(computeBayesianMetaAnalysis(populations)[, 1:3])
 
+limits = c(0.1, 10)
+breaks <- c(0.1, 0.25, 0.5, 1, 2, 4, 6, 8, 10)
+plotData <- adaptiveGridApproximations[[10]]
+plot <- ggplot2::ggplot(plotData, ggplot2::aes(x = point, y = value)) +
+  ggplot2::geom_vline(xintercept = log(breaks), color = "#AAAAAA", lty = 1, linewidth = 0.5) +
+  ggplot2::geom_line(alpha = 0.2) +
+  ggplot2::geom_point(size = 0.5, alpha = 0.5) +
+  ggplot2::scale_y_continuous("Log Likelihood") +
+  ggplot2::scale_x_continuous("Hazard Ratio",
+                              limits = log(limits),
+                              breaks = log(breaks),
+                              labels = breaks) +
+  ggplot2::theme(panel.grid.minor = ggplot2::element_blank(),
+                 panel.background = ggplot2::element_blank(),
+                 panel.grid.major = ggplot2::element_blank(),
+                 legend.title = ggplot2::element_blank(),
+                 axis.ticks.x = ggplot2::element_blank(),
+                 legend.position = "top")
+plot
+ggplot2::ggsave(filename = "d:/temp/plot.png", plot = plot, width = 6, height = 3, dpi = 200)
