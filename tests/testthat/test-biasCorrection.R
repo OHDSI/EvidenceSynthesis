172bd7e1a11ebd5aed3cee452131a3de32f809f2
fanbu1995 <fb75@duke.edu>
2023-04-27 19:48:24 -0700

    add unit tests for bias correction functions

---
+++ tests/testthat/test-biasCorrection.R
@@ -0,0 +1,59 @@
+library(testthat)
+library(EvidenceSynthesis)
+
+# load the example likelihoods
+data("ncLikelihoods")
+data("ooiLikelihoods")
+
+test_that('fit bias distribution', {
+  # normal model
+  biasDist5 = fitBiasDistribution(ncLikelihoods[[5]], numsamps = 1000)
+
+  expect_type(biasDist5, "list")
+  expect_named(biasDist5, c('mean', 'scale', 'bias'))
+  expect_type(biasDist5$bias, 'double')
+
+  # t model
+  biasDist5 = fitBiasDistribution(ncLikelihoods[[5]], numsamps = 1000, robust = TRUE)
+
+  expect_type(biasDist5, "list")
+  expect_named(biasDist5, c('mean', 'scale', 'bias'))
+  expect_type(biasDist5$bias, 'double')
+})
+
+test_that('sequentially fit bias distribution', {
+  biasDist = sequentialFitBiasDistribution(ncLikelihoods, numsaps = 1000)
+
+  expect_type(biasDist, "list")
+  expect_named(biasDist, c('mean', 'scale', 'bias', 'Id'))
+  expect_type(biasDist$bias, 'double')
+})
+
+test_that('Bayesian bias correction', {
+  bbcSequential = biasCorrectionInference(ooiLikelihoods,
+                                          ncLikelihoods,
+                                          numsamps = 1000,
+                                          doCorrection = TRUE)
+
+  expect_type(bbcSequential, "list")
+  expect_names(bbcSequential, c("median", "mean", "ci95Lb", "ci95Ub", "p1", "Id"))
+  expect_gte(bbcSequential$ci95Ub, bbcSequential$median)
+  expect_lte(bbcSequential$ci95Lb, bbcSequential$median)
+})
+
+test_that('plot bias distribution', {
+  biasDist = sequentialFitBiasDistribution(ncLikelihoods, numsaps = 5000)
+  biasPlot = plotBiasDistribution(biasDist)
+
+  expect_s3_class(biasPlot, "ggplot")
+})
+
+test_that("plot bias correction", {
+  bbcSequential = biasCorrectionInference(ooiLikelihoods,
+                                          ncLikelihoods,
+                                          numsamps = 5000,
+                                          doCorrection = TRUE)
+  bbcPlot = plotBiasCorrectionInference(bbcSequential)
+
+  expect_s3_class(bbcPlot, "ggplot")
+})
