3a059edf94064e01fdf04acb0e15ff741bc5f3eb
Fan Bu <fanbu@Fans-MacBook-Pro-2.local>
2024-01-29 20:17:56 -0500

    add test scripts for new functions

--- tests/testthat/test-hierarchicalMetaAnalysis.R
+++ tests/testthat/test-hierarchicalMetaAnalysis.R
@@ -1,6 +1,23 @@
 library(testthat)
 #library(EvidenceSynthesis)
 
+## test simulation function
+test_that('simulate with negative controls on multiple databases', {
+  metaPopulations = simulateMetaAnalysisWithNegativeControls(meanExposureEffect = log(2.0),
+                                                             mNegativeControls = 10,
+                                                             sitePop = 1000,
+                                                             treatedFraction = 0.5,
+                                                             minBackgroundHazard = 0.2,
+                                                             maxBackgroundHazard = 0.2,
+                                                             nStrata = 1,
+                                                             meanBias = 0.5, biasStd = 0.1,
+                                                             siteEffectStd = 0.15,
+                                                             nSites = 5)
+  expect_type(metaPopulations, "list")
+  expect_type(attr(metaPopulations, "hyperParameters"), "list")
+
+})
+
 ## load the example data (list of likelihood profiles across outcomes and data sources)
 data("hmaLikelihoodList")
 
@@ -75,7 +92,9 @@ test_that("run hierarchical meta analysis without main exposure effect", {
                                                             subSampleFrequency = 10,
                                                             includeExposureEffect = FALSE)
 
-  estimates = EvidenceSynthesis::computeHierarchicalMetaAnalysis(data = likelihoodProfileLists,
+  likelihoodProfileLists_ncOnly = likelihoodProfileLists[1:10]
+
+  estimates = EvidenceSynthesis::computeHierarchicalMetaAnalysis(data = likelihoodProfileLists_ncOnly,
                                                                  settings = settings,
                                                                  seed = 666)
 
@@ -84,4 +103,30 @@ test_that("run hierarchical meta analysis without main exposure effect", {
   expect_type(attr(estimates, "traces"), "double")
   expect_false("exposure" %in% estimates$parameter)
 })
+
+test_that("run hierarchical meta analysis without two main exposure effects", {
+  settings = EvidenceSynthesis::generateBayesianHMAsettings(chainLength = 110000,
+                                                            burnIn = 1e+04,
+                                                            subSampleFrequency = 10,
+                                                            includeExposureEffect = TRUE,
+                                                            exposureEffectCount = 2,
+                                                            globalExposureEffectPriorMean = c(1.0, 1.5),
+                                                            globalExposureEffectPriorStd = c(2.0, 10.0))
+
+  likelihoodProfileLists_twoExposures = c(likelihoodProfileLists[1:10],
+                                          likelihoodProfileLists[11],
+                                          likelihoodProfileLists[11])
+
+  estimates = EvidenceSynthesis::computeHierarchicalMetaAnalysis(data = likelihoodProfileLists_twoExposures,
+                                                                 settings = settings,
+                                                                 seed = 666)
+
+  expect_type(estimates, "list")
+  expect_named(estimates, c("mean", "median", "LB", "UB", "se", "parameter"))
+  expect_type(attr(estimates, "traces"), "double")
+  expect_true("exposure1" %in% estimates$parameter)
+  expect_true("exposure2" %in% estimates$parameter)
+})
+
+
 
